3874bc48c539c9fe7177fa00e861b9f448b07932
LuisFlores2121 <114443206+LuisFlores2121@users.noreply.github.com>
2024-05-20 15:36:09 -0600

    Add files via upload

---
+++ Programas/Practica 1 CSharp enviar mensaje a picow/Interfaz Practica 1/Interfaz Practica 1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Interfaz_Practica_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxPuertos = new System.Windows.Forms.ComboBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtBoxMensaje = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxPuertos
+            // 
+            this.comboBoxPuertos.FormattingEnabled = true;
+            this.comboBoxPuertos.Location = new System.Drawing.Point(23, 48);
+            this.comboBoxPuertos.Name = "comboBoxPuertos";
+            this.comboBoxPuertos.Size = new System.Drawing.Size(183, 24);
+            this.comboBoxPuertos.TabIndex = 0;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(12, 122);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 1;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Location = new System.Drawing.Point(95, 122);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(75, 23);
+            this.btnDesconectar.TabIndex = 2;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            this.btnDesconectar.Click += new System.EventHandler(this.btnDesconectar_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(238, 122);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtBoxMensaje
+            // 
+            this.txtBoxMensaje.Location = new System.Drawing.Point(23, 181);
+            this.txtBoxMensaje.Name = "txtBoxMensaje";
+            this.txtBoxMensaje.ReadOnly = true;
+            this.txtBoxMensaje.Size = new System.Drawing.Size(409, 22);
+            this.txtBoxMensaje.TabIndex = 4;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(357, 122);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 5;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 450);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtBoxMensaje);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.comboBoxPuertos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxPuertos;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtBoxMensaje;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
